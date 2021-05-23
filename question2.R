cecac154643f43fed7c0e4829d69d5248cde07a8
diwakar777 <79821949+diwakar777@users.noreply.github.com>
2021-05-23 16:21:26 +0530

    Create question2.R

---
+++ question2.R
@@ -0,0 +1,18 @@
+m1 = matrix(1:9,3,3)
+print("Matrix-1:")
+print(m1)
+m2 = matrix(1:9,3,3)
+print("Matrix-2:")
+print(m2)
+
+result = m1 + m2
+print("Result of addition")
+print(result)
+
+result = m1 - m2
+print("Result of subtraction")
+print(result)
+
+result = m1 * m2
+print("Result of multiplication")
+print(result)
